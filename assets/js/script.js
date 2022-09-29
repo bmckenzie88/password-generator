a2e5e61f8a92b028607ac6c2272048f7a2f0bbaf
Bryce McKenzie <brycemckenzie@Roses-MacBook-Air.local>
2022-09-29 14:54:45 -0700

    created randomInt variable and got result to return in password generator

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,79 +2,99 @@
 var generateBtn = document.querySelector("#generate");
 
 // global variables
-var result = ""
+var result = "";
 
 // array of numbers, specials, uppercase, lowercase
-var numbers = [0,1,2,3,4,5,6,7,8,9]
-console.log(numbers)
-var specials = ["!","@","#","$","%","^","&","*"]
-console.log(specials)
-var upcase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-console.log(upcase)
-var lowcase = upcase.map(element => {
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(numbers);
+var specials = ["!", "@", "#", "$", "%", "^", "&", "*"];
+console.log(specials);
+var upcase = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+console.log(upcase);
+var lowcase = upcase.map((element) => {
   return element.toLowerCase();
 });
-console.log(lowcase)
-var optionsPool = lowcase
-
-
+console.log(lowcase);
+var optionsPool = lowcase;
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector ("#password");
+  var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 function generatePassword() {
-// build random password
-// prompt user for length
-var lengthChoice = prompt("How many characters?")
-// check length,
-console.log(lengthChoice)
-// prompt/confirm for types of characters
-var numberChoice = confirm("Include numbers?")
-console.log("include numbers - " + numberChoice)
-var specialsChoice = confirm("Include special characters?")
-console.log("include special characters - " + specialsChoice)
-var upCaseChoice = confirm("Include uppercase letters?")
-console.log("include uppercase letters - " + upCaseChoice)
+  // build random password
+  // prompt user for length
+  var lengthChoice = prompt("How many characters?");
+  // check length,
+  console.log(lengthChoice);
+  // prompt/confirm for types of characters
+  var numberChoice = confirm("Include numbers?");
+  console.log("include numbers - " + numberChoice);
+  var specialsChoice = confirm("Include special characters?");
+  console.log("include special characters - " + specialsChoice);
+  var upCaseChoice = confirm("Include uppercase letters?");
+  console.log("include uppercase letters - " + upCaseChoice);
 
-// store responses
-// based on responses trim/build customized pools of characters
-    if (numberChoice) {
-        optionsPool = optionsPool.concat(numbers)
-    }
-    if (specialsChoice) {
-       optionsPool = optionsPool.concat(specials)
-    }
-    if (upCaseChoice) {
-        optionsPool = optionsPool.concat(upcase)
-    }
-    console.log(optionsPool)
+  // store responses
+  // based on responses trim/build customized pools of characters
+  if (numberChoice) {
+    optionsPool = optionsPool.concat(numbers);
+  }
+  if (specialsChoice) {
+    optionsPool = optionsPool.concat(specials);
+  }
+  if (upCaseChoice) {
+    optionsPool = optionsPool.concat(upcase);
+  }
+  console.log(optionsPool);
+
+  // build password character by character
+  // generate random Int within range of available char length
+  var randomInt = Math.floor(Math.random() * optionsPool.length);
+  console.log(randomInt);
+  // add char to result
+  var result = optionsPool[randomInt];
+
+  // repeat until decided length is met
+  for (i = 0; i < length; i++) {
+    result += optionsPool[randomInt];
+  }
+  return result;
 }
-
-// build password character by character
-// generate random Int within range of available char length
-
-// add char to result
-// repeat until decided length is met
-//   return result
-// }
-
-
-
-
-
-
-
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
 
 // alert("hello!")
 // var isHappy = confirm("are you happy?")
@@ -84,13 +104,8 @@ generateBtn.addEventListener("click", writePassword);
 
 // random=(math.floor(Math.random()*100) +1)
 
-
-
-
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
 
 // alert("hello!")
 // var isHappy = confirm("are you happy?")
